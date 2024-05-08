f162c7e6383562dfe4ab3055facd5fcb182a3201
Michael <michael.boitmann@gmail.com>
2024-05-08 23:29:07 +1200

    change the semantics of navigation bar from div to nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
-import bg from "../../assets/images/bg.png"
+import bg from "../../assets/images/bg.png";
+import bag from "../../assets/images/bag.png";
 
 const Header = () => {
   return (
     <div className="w-full md:h-[110vh] h-screen bg-cover relative bg-center md:px-[80px] px-8 py-[30px]" 
       style={{backgroundImage:`url(${bg})`}}>
-        <div className="w-full flex items-center justify-between">
+        <nav className="w-full flex items-center justify-between">
           <span className="text-white font-bold text-[68px] cursor-pointer">Muebles</span>
-          <nav className="hidden md:flex items-center gap-[62px]">
+          <div className="hidden md:flex items-center gap-[62px]">
             <a href="/" 
               className="text-white no-underline font-medium text-[18px] transition-all duration-300
               hover:text-[#35E644]">
@@ -27,8 +28,9 @@ const Header = () => {
               hover:text-[#35E644]">
               Contact
             </a>
-          </nav>
-        </div>
+          </div>
+
+        </nav>
 
     </div>
   )
