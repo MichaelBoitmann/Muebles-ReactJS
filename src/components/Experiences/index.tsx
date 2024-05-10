79d1f24f3898f6e2ad887f47514b18255def98b6
Michael <michael.boitmann@gmail.com>
2024-05-10 23:08:02 +1200

    created an experience background, header and statement

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -5,6 +5,11 @@ import experiences from "../../assets/images/sofa1.png";
 const Experiences = () => {
   return (
     <div className="w-full flex md:flex-row flex-col items-center md:gap-48 gap-20 mt-[240px] mb-[120px]">
+      <div className="relative">
+        <div className="absolute bg-[#f7f7f7] rounded-tr-[20px] left-0 top-[-66px] w-[450px] h-[300px] z-[-1]"></div>
+        <img src={experiences} alt="experiences" />
+        <div className="absolute bg-[#f7f7f7] rounded-[20px] right-0 top-[45%] -translate-y-1/2 translate-x-[50px] w-[450px] h-[300px] z-[-1]"></div>
+      </div>
 
     </div>
   );
