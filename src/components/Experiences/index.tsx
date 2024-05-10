f29278e329534b55962cf0947606096e1115ccf0
Michael <michael.boitmann@gmail.com>
2024-05-10 23:09:09 +1200

    reword statement and header

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -10,7 +10,19 @@ const Experiences = () => {
         <img src={experiences} alt="experiences" />
         <div className="absolute bg-[#f7f7f7] rounded-[20px] right-0 top-[45%] -translate-y-1/2 translate-x-[50px] w-[450px] h-[300px] z-[-1]"></div>
       </div>
-
+      <div className="flex flex-col mt-[-60px] md:px-0 px-8">
+        <span className="font-medium text-[#e58411] text-[18px] tracking-[0.2em]">
+          Experiences
+        </span>
+        <div className="font-bold text-[#1e1e1e] text-[42px] max-w-[450px] leading-[50px] mt-5">
+          We Provide You The Best Experience
+        </div>
+        <p className="mt-5 max-w-[500px]">
+          You don't have to worry about the result because all of these interiors are made by
+          people who are professionals in their fields with an elegant and lucurious style and
+          with premium quality materials.
+        </p>
+      </div>
     </div>
   );
 };
