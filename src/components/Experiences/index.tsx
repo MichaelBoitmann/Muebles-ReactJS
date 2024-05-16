5a44c7436d56e348b822afda23ff7a5ba7c6f5fb
Michael <michael.boitmann@gmail.com>
2024-05-16 21:51:41 +1200

    finetuning of each section

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -4,12 +4,13 @@ import experiences from "../../assets/images/sofa1.75.png";
 
 const Experiences = () => {
   return (
-    <div className="w-full flex md:flex-row flex-col items-center justify-center md:gap-48 gap-20 mt-[240px] mb-[120px]">
+    <div className="w-full flex md:flex-row flex-col items-center md:gap-48 gap-20 mt-[140px] mb-[120px]">
       <div className="relative">
         <div className="absolute bg-[#f7f7f7] rounded-tr-[20px] left-0 top-[-66px] w-[450px] h-[300px] z-[-1]"></div>
         <img 
           src={experiences} 
           alt="experiences"
+          className="rounded-r-[20px]"
         />
         <div className="absolute bg-[#f7f7f7] rounded-[20px] right-0 top-[45%] -translate-y-1/2 translate-x-[50px] w-[450px] h-[300px] z-[-1]"></div>
       </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,11 @@ import bag from "../../assets/images/bag.png";
 const Header = () => {
   return (
     <div>
-      <div className="w-full md:h-[105vh] h-screen bg-cover relative bg-center md:px-[80px] px-8 py-[40px]" 
+      <div className="w-full md:h-[110vh] h-screen bg-cover relative bg-center md:px-[80px] px-8 py-[40px]" 
            style={{backgroundImage:`url(${bg})`}}>
         <div className="w-full flex items-center justify-between">
           <span className="text-white font-bold text-[28px] cursor-pointer">Muebles</span>
-          <div className="hidden md:flex items-center gap-[62px]">
+          <div className="hidden md:flex items-center object-right gap-[62px]">
             <a href="/" 
               className="text-white no-underline font-medium text-[18px] transition-all duration-300
               hover:text-[#35E644]">
@@ -33,15 +33,15 @@ const Header = () => {
           <img src={bag} alt="cart" className="text-white cursor-pointer w-[25px]" />
         </div>
         <div className="w-full flex flex-col gap-5 items-center justify-center md:mt-[100px] mt-5">
-          <div className="text-center text-white font-bold md:text-[80px] 
-          text-[60px] md:max-w-[900px] leading-[90px]">
+          <div className="text-center text-white font-bold md:text-[70px] 
+          text-[40px] md:max-w-[900px] leading-[90px]">
             Make Your Interior More Minimalistic & Modern
           </div>
-          <p className="text-white md:text-[24px] text-[18px] font-[200] max-w-[600] text-center">
+          <p className="text-white md:text-[24px] text-[18px] font-[200] max-[300] text-center">
             Turn your room with Muebles into a lot more minimalist and modern with ease and speed.
           </p>
         </div>
-        <div className="w-full h-[80px] bg-gradient-to-t from-white to-transparent abolute bottom-0 left-0"></div>
+        <div className="w-full h-[80px] bg-gradient-to-t from-white to-transparent abolute left-0 mt-[12%]"></div>
       </div>
       
     </div>
